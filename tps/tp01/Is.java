c00c600dc2c813df8adf7c009c469cff187c42c2
Samuel Elias <samuelelias563@gmail.com>
2026-03-13 15:28:22 -0300

    questão 3 (não consigo de jeito nenhum)

---
+++ tps/tp01/Is.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class Is {
+  public static boolean isSomenteVogais(String s) {
+    char[] v = { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+
+    for (int i = 0; i < s.length(); i++) {
+      int dir = v.length - 1, esq = 0, meio;
+      boolean resp = false;
+
+      // aqui eu aplico a busca binária para ver se o caractere atual consta no array
+      // de vogais (as vogais estão ordenadas conforme o padrão ascii)
+      while (esq <= dir) {
+        meio = (dir + esq) / 2;
+        int diff = (int) s.charAt(i) - (int) v[meio];
+
+        if (diff == 0) {
+          resp = true;
+          esq = v.length;
+        } else if (diff > 0) {
+          esq = meio + 1;
+        } else {
+          dir = meio - 1;
+        }
+      }
+
+      if (!resp) // se depois da pesquisa binaria eu nao ter identificado uma vogal, nem
+                 // precisa seguir com o procedimento
+        return false;
+
+    }
+
+    return true;
+  }
+
+  public static boolean isSomenteConsoantes(String s) {
+    char[] c = {
+        'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'b',
+        'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+
+    };
+
+    for (int i = 0; i < s.length(); i++) {
+      int dir = c.length - 1, esq = 0, meio;
+      boolean resp = false;
+      // aqui eu aplico a busca binária para ver se o caractere atual consta no array
+      // de consoantes (as consoantes estão ordenadas conforme o padrão ascii)
+      while (esq <= dir) {
+        meio = (dir + esq) / 2;
+
+        int diff = (int) s.charAt(i) - (int) c[meio];
+
+        if (diff == 0) {
+          resp = true;
+          esq = c.length;
+        } else if (diff > 0) {
+          esq = meio + 1;
+        } else {
+          dir = meio - 1;
+        }
+      }
+
+      if (!resp) // se depois da pesquisa binaria eu nao ter identificado uma consoante, nem
+                 // precisa seguir com o procedimento
+        return false;
+
+    }
+
+    return true;
+  }
+
+  public static boolean isNumeroInteiro(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (!(c >= '0' && c <= '9')) // '0' é 48 e '9' é 57
+        return false;
+    }
+    return true;
+  }
+
+  // a implementação desse método é identica ao anterior porque Z é um subconjunto
+  // de R
+  // public static boolean isNumeroReal(String s) {
+  // for (int i = 0; i < s.length(); i++) {
+  // if (!(s.charAt(i) >= 30 && s.charAt(i) <= 39)) // se o caractere atual não
+  // estiver entre esse range, pode parar!
+  // return false;
+
+  // }
+
+  // return true;
+  // }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    String input = s.nextLine();
+    while (!(input.charAt(0) == 'F' && input.charAt(1) == 'I' && input.charAt(2) == 'M')) {
+      System.out.print(isSomenteVogais(input) ? "SIM " : "NAO ");
+      System.out.print(isSomenteConsoantes(input) ? "SIM " : "NAO ");
+      System.out.print(isNumeroInteiro(input) ? "SIM " : "NAO ");
+      // System.out.print(isNumeroReal(input) ? "SIM \n" : "NAO \n");
+      System.out.println();
+      input = s.nextLine();
+    }
+    s.close();
+  }
+
+}
