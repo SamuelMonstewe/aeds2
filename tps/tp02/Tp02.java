2fd84bf2c900590fb87e874c42156b0bc6afc970
Samuel Elias <samuelelias563@gmail.com>
2026-04-07 15:29:17 -0300

    implementação da classe restaurante

--- tps/tp02/Tp02.java
+++ tps/tp02/Tp02.java
@@ -1,26 +1,159 @@
 import java.util.Scanner;
 
+class Restaurante {
+  private int id;
+  private String nome;
+  private String cidade;
+  private int capacidade;
+  private double notaMedia;
+  private String[] tipoCozinha;
+  private int faixaPreco;
+  private Hora horarioAbertura;
+  private Hora horarioFechamento;
+  private Data dataAbertura;
+  private boolean aberto;
+
+  public Restaurante(
+      int id,
+      String nome,
+      String cidade,
+      int capacidade,
+      double notaMedia,
+      String[] tipoCozinha,
+      int faixaPreco,
+      Hora horarioAbertura,
+      Hora horarioFechamento,
+      Data dataAbertura,
+      boolean aberto) {
+
+    this.id = id;
+    this.nome = nome;
+    this.cidade = cidade;
+    this.capacidade = capacidade;
+    this.notaMedia = notaMedia;
+    this.tipoCozinha = tipoCozinha;
+    this.faixaPreco = faixaPreco;
+    this.horarioAbertura = horarioAbertura;
+    this.horarioFechamento = horarioFechamento;
+    this.dataAbertura = dataAbertura;
+    this.aberto = aberto;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(final int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(final String nome) {
+    this.nome = nome;
+  }
+
+  public String getCidade() {
+    return cidade;
+  }
+
+  public void setCidade(final String cidade) {
+    this.cidade = cidade;
+  }
+
+  public int getCapacidade() {
+    return capacidade;
+  }
+
+  public void setCapacidade(final int capacidade) {
+    this.capacidade = capacidade;
+  }
+
+  public double getNotaMedia() {
+    return notaMedia;
+  }
+
+  public void setNotaMedia(final double notaMedia) {
+    this.notaMedia = notaMedia;
+  }
+
+  public String[] getTipoCozinha() {
+    return tipoCozinha;
+  }
+
+  public void setTipoCozinha(final String[] tipoCozinha) {
+    this.tipoCozinha = tipoCozinha;
+  }
+
+  public int getFaixaPreco() {
+    return faixaPreco;
+  }
+
+  public void setFaixaPreco(final int faixaPreco) {
+    this.faixaPreco = faixaPreco;
+  }
+
+  public Hora getHorarioAbertura() {
+    return horarioAbertura;
+  }
+
+  public void setHorarioAbertura(final Hora horarioAbertura) {
+    this.horarioAbertura = horarioAbertura;
+  }
+
+  public Hora getHorarioFechamento() {
+    return horarioFechamento;
+  }
+
+  public void setHorarioFechamento(final Hora horarioFechamento) {
+    this.horarioFechamento = horarioFechamento;
+  }
+
+  public Data getDataAbertura() {
+    return dataAbertura;
+  }
+
+  public void setDataAbertura(final Data dataAbertura) {
+    this.dataAbertura = dataAbertura;
+  }
+
+  public boolean isAberto() {
+    return aberto;
+  }
+
+  public void setAberto(final boolean aberto) {
+
+    this.aberto = aberto;
+  }
+
+  public static Restaurante parseRestaurante(final String s) {
+    return new Restaurante(s);
+  }
+}
+
 class Hora {
   private int hora;
   private int minuto;
 
-  public Hora(int hora, int minuto) {
+  public Hora(final int hora, final int minuto) {
     this.hora = hora;
     this.minuto = minuto;
   }
 
-  public void setHora(int hora) {
+  public void setHora(final int hora) {
     this.hora = hora;
   }
 
-  public void setMinuto(int minuto) {
+  public void setMinuto(final int minuto) {
     this.minuto = minuto;
   }
 
-  public static Hora parseHora(String s) {
-    Scanner sc = new Scanner(s).useDelimiter(":");
-    int hora = sc.nextInt();
-    int minuto = sc.nextInt();
+  public static Hora parseHora(final String s) {
+    final Scanner sc = new Scanner(s).useDelimiter(":");
+    final int hora = sc.nextInt();
+    final int minuto = sc.nextInt();
     sc.close();
 
     return new Hora(hora, minuto);
@@ -36,29 +169,29 @@ class Data {
   private int mes;
   private int dia;
 
-  public Data(int ano, int mes, int dia) {
+  public Data(final int ano, final int mes, final int dia) {
     this.ano = ano;
     this.mes = mes;
     this.dia = dia;
   }
 
-  public void setAno(int ano) {
+  public void setAno(final int ano) {
     this.ano = ano;
   }
 
-  public void setMes(int mes) {
+  public void setMes(final int mes) {
     this.mes = mes;
   }
 
-  public void setDia(int dia) {
+  public void setDia(final int dia) {
     this.dia = dia;
   }
 
-  public static Data parseData(String s) {
-    Scanner sc = new Scanner(s).useDelimiter("-");
-    int ano = sc.nextInt();
-    int mes = sc.nextInt();
-    int dia = sc.nextInt();
+  public static Data parseData(final String s) {
+    final Scanner sc = new Scanner(s).useDelimiter("-");
+    final int ano = sc.nextInt();
+    final int mes = sc.nextInt();
+    final int dia = sc.nextInt();
     sc.close();
 
     return new Data(ano, mes, dia);
@@ -70,8 +203,8 @@ class Data {
 }
 
 class Tp02 {
-  public static void main(String[] args) {
-    Hora hora = Hora.parseHora("14:45");
+  public static void main(final String[] args) {
+    final Hora hora = Hora.parseHora("14:45");
     System.out.println(hora.formatar());
   }
 }
