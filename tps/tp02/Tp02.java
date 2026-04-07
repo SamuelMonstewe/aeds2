18d9dfbad5f5feda5032253067fc4ca3afa8cbee
Samuel Elias <samuelelias563@gmail.com>
2026-04-07 14:26:00 -0300

    iniciando trabalho prático II

---
+++ tps/tp02/Tp02.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Data {
+  private int ano;
+  private int mes;
+  private int dia;
+
+  public Data(int ano, int mes, int dia) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public void setMes(int mes) {
+    this.mes = mes;
+  }
+
+  public void setDia(int dia) {
+    this.dia = dia;
+  }
+
+  public static Data parseData(String s) {
+    Scanner sc = new Scanner(s).useDelimiter("-");
+    int ano = sc.nextInt();
+    int mes = sc.nextInt();
+    int dia = sc.nextInt();
+    sc.close();
+
+    return new Data(ano, mes, dia);
+  }
+
+  public String formatar() {
+    return String.format("%d/%d/%d", dia, mes, ano);
+  }
+}
+
+class Tp02 {
+  public static void main(String[] args) {
+    Data data = Data.parseData("2004-09-23");
+    System.out.println(data.formatar());
+  }
+}
