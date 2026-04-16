6eddc0fdf02f1160f53a3886eecbe4e7a10f561e
Samuel Elias <samuelelias563@gmail.com>
2026-04-16 16:13:10 -0300

    função parse restaurante

--- tps/tp02/Tp02.c
+++ tps/tp02/Tp02.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 typedef struct {
   int hora;
   int minuto;
@@ -34,12 +34,12 @@ void formatar_data(Data *data, char *buffer) {
 
 typedef struct {
   int id;
-  char *nome;
-  char *cidade;
+  char nome[100];
+  char cidade[100];
   int capacidade;
   double avaliacao;
   int n_tipos_cozinha;
-  char **tipos_cozinha;
+  char *tipos_cozinha[2];
   int faixa_preco;
   Hora horario_abertura;
   Hora horario_fechamento;
@@ -47,7 +47,59 @@ typedef struct {
   int aberto;
 } Restaurante;
 
-Restaurante *parse_restaurante(char *s) {}
+Restaurante *parse_restaurante(char *s) {
+  Restaurante *r = (Restaurante *)malloc(sizeof(Restaurante));
+  char strTiposCozinha[50];
+  char strFaixaPreco[5];
+  char strHorario[12];
+  char strDataAbertura[11];
+  char strAberto[6];
+
+  sscanf(s, "%d,%[^,],%[^,],%d,%lf,%[^,],%[^,],%[^,],%[^,],%[^,]", &r->id,
+         r->nome, r->cidade, &r->capacidade, &r->avaliacao, strTiposCozinha,
+         strFaixaPreco, strHorario, strDataAbertura, strAberto);
+
+  strHorario[5] = '\0';
+
+  r->horario_abertura = parse_hora(strHorario);
+  r->horario_fechamento = parse_hora(strHorario + 6);
+  r->data_abertura = parse_data(strDataAbertura);
+  r->aberto = strcmp("true", strAberto) ? 0 : 1;
+
+  char t1[30];
+  char t2[30];
+
+  sscanf(strTiposCozinha, "%[^;];%[^\n]", t1, t2);
+
+  int tam1 = 0;
+  while (t1[tam1] != '\0') {
+    tam1++;
+  }
+
+  int tam2 = 0;
+  while (t2[tam2] != '\0') {
+    tam2++;
+  }
+
+  r->tipos_cozinha[0] = (char *)malloc(tam1 + 1 * sizeof(char));
+  r->tipos_cozinha[1] = (char *)malloc(tam2 + 1 * sizeof(char));
+
+  for (int i = 0; i < tam1; i++) {
+    r->tipos_cozinha[0][i] = t1[i];
+  }
+
+  r->tipos_cozinha[0][tam1] = '\0';
+
+  for (int i = 0; i < tam2; i++) {
+    r->tipos_cozinha[1][i] = t2[i];
+  }
+
+  r->tipos_cozinha[1][tam2] = '\0';
+
+  // printf("%s %s %s %s %s %s %d", r->tipos_cozinha[0], r->tipos_cozinha[1],
+  //        strFaixaPreco, strHorario, strDataAbertura, strAberto, r->aberto);
+  return r;
+}
 void formatar_restaurante(Restaurante *restaurante, char *buffer) {}
 
 typedef struct {
@@ -58,4 +110,9 @@ typedef struct {
 void ler_csv_colecao(Colecao_Restaurantes *c, char *path) {}
 Colecao_Restaurantes *ler_csv() {}
 
-int main() {}
+int main() {
+  char s[100] = "1,Classic Palace "
+                "Works,Zurich,168,3.9,churrasco;internacional,$$,11:00-20:00,"
+                "2018-03-31,false";
+  parse_restaurante(s);
+}
