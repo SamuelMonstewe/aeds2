d1282f69e3392c19fd2545c8b3f445582f622eef
Samuel Elias <samuelelias563@gmail.com>
2026-05-04 18:56:25 -0300

    questão 6

--- tps/tp03/Tp03.java
+++ tps/tp03/Tp03.java
@@ -356,304 +356,49 @@ class Data {
 }
 
 class Celula {
-  Celula prox;
   Restaurante elemento;
+  Celula prox;
 }
 
-class Lista {
-  Celula primeiro;
-  Celula ultimo;
+class Pilha {
+  Celula topo;
 
-  public Lista() {
-    Celula lista = new Celula();
-    primeiro = lista;
-    ultimo = lista;
+  public Pilha() {
+    topo = null;
   }
 
-  public void inserirInicio(Restaurante restaurante) {
-    primeiro.elemento = restaurante;
-    Celula novoCabeca = new Celula();
-    novoCabeca.prox = primeiro;
-    primeiro = novoCabeca;
-    novoCabeca.elemento = restaurante;
-    novoCabeca = null;
+  public void push(Restaurante r) {
+    Celula nova = new Celula();
+    nova.elemento = r;
+
+    nova.prox = topo;
+    topo = nova;
   }
 
-  public void inserir(Restaurante restaurante, int posicao) {
-    int tam = tamanho();
-    if (posicao < 0 || posicao > tam) {
-      return;
-    } else if (posicao == 0) {
-      inserirInicio(restaurante);
-    } else if (posicao == tam) {
-      inserirFim(restaurante);
-    } else {
-      Celula i = primeiro;
-      for (int j = 0; j < posicao; j++) {
-        i = i.prox;
-      }
-
-      Celula tmp = new Celula();
-      tmp.elemento = restaurante;
-      tmp.prox = i.prox;
-      i.prox = tmp;
-    }
-  }
-
-  public void inserirFim(Restaurante restaurante) {
-    Celula novo = new Celula();
-    novo.elemento = restaurante;
-    novo.prox = null;
-    ultimo.prox = novo;
-    ultimo = novo;
-    novo = null;
-  }
-
-  public Restaurante removerInicio() {
-    if (primeiro == ultimo) {
+  public Restaurante pop() {
+    if (topo == null) {
       return null;
     }
 
-    Celula tmp = primeiro.prox;
-    primeiro.prox = tmp.prox;
-    Restaurante r = tmp.elemento;
-    tmp.prox = null;
-    tmp = null;
-    return r;
+    Celula tmp = topo;
+    topo = tmp.prox;
+    return tmp.elemento;
   }
 
-  public int tamanho() {
-    Celula i = primeiro;
-    int n = 0;
-
-    while (i.prox != null) {
-      n++;
-      i = i.prox;
-    }
-
-    return n;
-  }
-
-  public Restaurante removerFim() {
-    if (primeiro == ultimo) {
-      return null;
-    }
-
-    Celula i = primeiro;
-
-    while (i.prox != ultimo) {
-      i = i.prox;
-    }
-
-    ultimo = i;
-    Restaurante r = i.prox.elemento;
-    ultimo.prox = null;
-    i = null;
-    return r;
-  }
-
-  public Restaurante remover(int posicao) {
-    int tam = tamanho();
-    Restaurante r;
-    if (primeiro == ultimo || tam < 0 || posicao >= tam) {
-      return null;
-    } else if (posicao == 0) {
-      r = removerInicio();
-    } else if (posicao == tam - 1) {
-      r = removerFim();
-    } else {
-      Celula i = primeiro;
-      int j = 0;
-
-      while (j < posicao) {
-        i = i.prox;
-        j++;
-      }
-
-      Celula tmp = i.prox;
-      i.prox = tmp.prox;
-      r = tmp.elemento;
-      tmp.prox = null;
-      tmp = null;
-    }
-
-    return r;
-  }
-
-  public void exibir() {
-    Celula i = primeiro.prox;
+  public void mostrar() {
+    Celula i = topo;
 
     while (i != null) {
       System.out.println(i.elemento.formatar());
       i = i.prox;
     }
   }
-
-}
-
-/*
- * 0 % 5 = 0
- * 1 % 5 = 1
- * 5 % 5 = 0
- */
-class Fila {
-  public Restaurante[] fila;
-  public int inicio;
-  public int fim;
-  public int n;
-  public int tam;
-
-  public Fila(int tam) {
-    fila = new Restaurante[tam];
-    this.tam = tam;
-    this.inicio = 0;
-    this.fim = 0;
-    this.n = 0;
-  }
-
-  public Restaurante dequeue() {
-    Restaurante r = fila[inicio];
-    n--;
-    inicio = (inicio + 1) % tam;
-    return r;
-  }
-
-  public void enqueue(Restaurante r) {
-    if (this.tam == n) {
-      System.out.println("(R)" + dequeue().getNome());
-    }
-    fila[fim] = r;
-    fim = (fim + 1) % tam;
-    n++;
-
-  }
-
-  public void imprimir() {
-    int pos = inicio;
-    for (int i = 0; i < n; i++) {
-      System.out.println(fila[pos].formatar());
-
-      pos = (pos + 1) % tam;
-    }
-  }
-
-  public int getMediaAno() {
-    int pos = inicio, soma = 0;
-    for (int i = 0; i < n; i++) {
-      soma += fila[pos].getDataAbertura().getAno();
-
-      pos = (pos + 1) % tam;
-    }
-
-    return (int) Math.round((double) soma / n);
-  }
-}
-
-class ListaSequencial {
-  Restaurante[] arr;
-  int n;
-
-  public ListaSequencial() {
-    arr = new Restaurante[200];
-    n = 0;
-  }
-
-  public void inserirInicio(Restaurante r) {
-    if (arr.length == n) {
-      return;
-    }
-
-    for (int i = n; i > 0; i--) {
-      arr[i] = arr[i - 1];
-    }
-    arr[0] = r;
-    n++;
-  }
-
-  public void inserirFim(Restaurante r) {
-    if (arr.length == n) {
-      return;
-    }
-
-    arr[n++] = r;
-  }
-
-  public void inserir(Restaurante r, int pos) {
-    if (arr.length == n) {
-      return;
-    }
-
-    for (int i = n; i > pos; i--) {
-      arr[i] = arr[i - 1];
-    }
-    arr[pos] = r;
-    n++;
-
-  }
-
-  public Restaurante removerInicio() {
-    if (n == 0) {
-      return null;
-    }
-
-    Restaurante r = arr[0];
-    n--;
-
-    for (int i = 0; i < n; i++) {
-      arr[i] = arr[i + 1];
-    }
-
-    return r;
-  }
-
-  public Restaurante removerFim() {
-    if (n == 0) {
-      return null;
-    }
-
-    Restaurante r = arr[--n];
-    return r;
-  }
-
-  public Restaurante remover(int pos) {
-    if (n == 0) {
-      return null;
-    }
-
-    Restaurante r = arr[pos];
-    n--;
-
-    for (int i = pos; i < n; i++) {
-      arr[i] = arr[i + 1];
-    }
-
-    return r;
-  }
-
-  public void exibir() {
-    for (int i = 0; i < n; i++) {
-      System.out.println(arr[i].formatar());
-    }
-  }
 }
 
 class Tp03 {
-  public static int compInsercao = 0;
-  public static double tempoInsercao = 0.0;
-  public static int movInsercao = 0;
 
   public static int compSequencial = 0;
   public static double tempoSequencial = 0.0;
-
-  public static int compMerge = 0;
-  public static double tempoMerge = 0.0;
-  public static int movMerge = 0;
-
-  public static int tamanhoHeap = 0;
-
-  public static int compHeap = 0;
-  public static double tempoHeap = 0.0;
-  public static int movHeap = 0;
 
   public static int compSelecao = 0;
   public static double tempoSelecao = 0.0;
@@ -686,159 +431,6 @@ class Tp03 {
 
     long fim = System.nanoTime();
     tempoSequencial += (fim - inicio) / 1000000.0;
-  }
-
-  public static void ordenacaoPorInsercao(Restaurante[] rs, int end) {
-    long tempoInicial = System.nanoTime();
-    int j;
-
-    for (int i = 1; i < end; i++) {
-      Restaurante chave = rs[i];
-      j = i - 1;
-      compInsercao++;
-      while ((j >= 0) && rs[j].getCidade().compareTo(chave.getCidade()) > 0) {
-        rs[j + 1] = rs[j];
-        movInsercao++;
-        j--;
-      }
-
-      movInsercao++;
-      rs[j + 1] = chave;
-    }
-
-    long tempoFinal = System.nanoTime();
-    tempoInsercao += (tempoFinal - tempoInicial) / 1000000.0;
-  }
-
-  public static void merge(Restaurante[] rs, int p, int q, int r) {
-    int nl = q - p + 1;
-    int nr = r - q, i, j;
-    Restaurante[] L = new Restaurante[nl];
-    Restaurante[] R = new Restaurante[nr];
-
-    for (i = 0; i < nl; i++) {
-      L[i] = rs[p + i];
-      movMerge++;
-    }
-    for (j = 0; j < nr; j++) {
-      R[j] = rs[q + j + 1];
-      movMerge++;
-    }
-
-    i = 0;
-    j = 0;
-    int k = p;
-
-    while (i < nl && j < nr) {
-      int diffCidade = L[i].getCidade().compareTo(R[j].getCidade());
-      compMerge++;
-      if (diffCidade == 0) { // se forem iguais, temos que desempatar
-        if (L[i].getNome().compareTo(R[j].getNome()) <= 0) { // desempate pelo nome
-          rs[k] = L[i];
-          movMerge++;
-          i++;
-        } else {
-          rs[k] = R[j];
-          movMerge++;
-          j++;
-        }
-      } else if (diffCidade < 0) {
-        rs[k] = L[i];
-        movMerge++;
-        i++;
-      } else {
-        rs[k] = R[j];
-        movMerge++;
-        j++;
-      }
-      k++;
-    }
-
-    while (i < nl) {
-      rs[k] = L[i];
-      i++;
-      k++;
-      movMerge++;
-    }
-    while (j < nr) {
-      rs[k] = R[j];
-      j++;
-      k++;
-      movMerge++;
-    }
-  }
-
-  public static void ordenacaoPorMerge(Restaurante[] rs, int p, int r) {
-    if (p >= r) {
-      return;
-    }
-    int q = (p + r) / 2;
-    ordenacaoPorMerge(rs, p, q);
-    ordenacaoPorMerge(rs, q + 1, r);
-    merge(rs, p, q, r);
-  }
-
-  public static int pai(int i) {
-    return (i - 1) / 2;
-  }
-
-  public static int esquerda(int i) {
-    return (2 * i) + 1;
-  }
-
-  public static int direita(int i) {
-    return (2 * i) + 2;
-  }
-
-  public static void maximizaHeap(Restaurante[] rs, int i) {
-    int l = esquerda(i);
-    int r = direita(i);
-    int maior = i;
-
-    if (l < tamanhoHeap) {
-      int comp = rs[l].getDataAbertura().compareTo(rs[maior].getDataAbertura());
-      compHeap += 2;
-      if ((comp > 0) || (comp == 0 && rs[l].getNome().compareTo(rs[maior].getNome()) > 0)) {
-        maior = l;
-      }
-    }
-
-    if (r < tamanhoHeap) {
-      int comp = rs[r].getDataAbertura().compareTo(rs[maior].getDataAbertura());
-      compHeap += 2;
-      if ((comp > 0) || (comp == 0 && rs[r].getNome().compareTo(rs[maior].getNome()) > 0)) {
-        maior = r;
-      }
-    }
-
-    if (maior != i) {
-      Restaurante tmp = rs[i];
-      rs[i] = rs[maior];
-      rs[maior] = tmp;
-      movHeap += 3;
-      maximizaHeap(rs, maior);
-    }
-  }
-
-  public static void constroiMaxHeap(Restaurante[] rs, int n) {
-    tamanhoHeap = n;
-
-    for (int i = (n / 2) - 1; i >= 0; i--) {
-      maximizaHeap(rs, i);
-    }
-  }
-
-  public static void ordenacaoPorHeap(Restaurante[] rs, int n) {
-    constroiMaxHeap(rs, n);
-
-    for (int i = n - 1; i > 0; i--) {
-      Restaurante tmp = rs[0];
-      rs[0] = rs[i];
-      rs[i] = tmp;
-      movHeap += 3;
-      tamanhoHeap--;
-      maximizaHeap(rs, 0);
-    }
   }
 
   public static void ordenacaoPorSelecao(Restaurante[] rs, int k, int n) {
@@ -892,6 +484,8 @@ class Tp03 {
   public static void quicksort(Restaurante[] rs, int p, int r, int k) {
     if (p < r) {
       int q = particiona(rs, p, r);
+      // Basta abandonar a partição à direita toda vez que a partição à esquerda
+      // contiver k ou mais itens (UFMG)
       if (q >= k) {
         quicksort(rs, p, q - 1, k);
       } else {
@@ -924,20 +518,48 @@ class Tp03 {
     // System.out.println(rs[i].formatar());
     // }
 
-    int end = 0;
+    // int end = 0;
+    // int id = s.nextInt();
+
+    // while (id != -1) {
+    // rs[end++] = pesquisaSequencialPorId(c, id);
+    // id = s.nextInt();
+    // }
+
+    // int k = 10;
+    // quicksort(rs, 0, end - 1, k);
+
+    // for (int i = 0; i < end; i++) {
+    // System.out.println(rs[i].formatar());
+    // }
+
+    Pilha pilha = new Pilha();
     int id = s.nextInt();
 
     while (id != -1) {
-      rs[end++] = pesquisaSequencialPorId(c, id);
+      pilha.push(pesquisaSequencialPorId(c, id));
       id = s.nextInt();
     }
 
-    int k = 10;
-    quicksort(rs, 0, end - 1, k);
+    int n = s.nextInt(), valor, size = 0;
+    Restaurante removidos[] = new Restaurante[100];
+    String comando;
 
-    for (int i = 0; i < end; i++) {
-      System.out.println(rs[i].formatar());
+    for (int i = 0; i < n; i++) {
+      comando = s.next();
+
+      if (comando.compareTo("I") == 0) {
+        valor = s.nextInt();
+        pilha.push(pesquisaSequencialPorId(c, valor));
+      } else if (comando.compareTo("R") == 0) {
+        removidos[size++] = pilha.pop();
+      }
     }
+
+    for (int j = 0; j < size; j++) {
+      System.out.println("(R)" + removidos[j].getNome());
+    }
+    pilha.mostrar();
     s.close();
   }
 }
