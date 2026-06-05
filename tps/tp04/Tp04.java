487f3f879b19bef4091fba3d38b97f8651349474
Samuel Elias <samuelelias563@gmail.com>
2026-06-05 14:55:39 -0300

    iniciando tp

---
+++ tps/tp04/Tp04.java
@@ -0,0 +1,445 @@
+import java.util.Scanner;
+
+class Restaurante {
+  private int id;
+  private String nome;
+  private String cidade;
+  private int capacidade;
+  private double notaMedia;
+  private String[] tiposCozinha;
+  private int faixaPreco;
+  private Hora horarioAbertura;
+  private Hora horarioFechamento;
+  private Data dataAbertura;
+  private boolean aberto;
+
+  public Restaurante(
+      int id,
+      String nome,
+      String cidade,
+      int capacidade,
+      double notaMedia,
+      String[] tiposCozinha,
+      int faixaPreco,
+      Hora horarioAbertura,
+      Hora horarioFechamento,
+      Data dataAbertura,
+      boolean aberto) {
+
+    this.id = id;
+    this.nome = nome;
+    this.cidade = cidade;
+    this.capacidade = capacidade;
+    this.notaMedia = notaMedia;
+    this.tiposCozinha = tiposCozinha;
+    this.faixaPreco = faixaPreco;
+    this.horarioAbertura = horarioAbertura;
+    this.horarioFechamento = horarioFechamento;
+    this.dataAbertura = dataAbertura;
+    this.aberto = aberto;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCidade() {
+    return cidade;
+  }
+
+  public void setCidade(String cidade) {
+    this.cidade = cidade;
+  }
+
+  public int getCapacidade() {
+    return capacidade;
+  }
+
+  public void setCapacidade(int capacidade) {
+    this.capacidade = capacidade;
+  }
+
+  public double getNotaMedia() {
+    return notaMedia;
+  }
+
+  public void setNotaMedia(double notaMedia) {
+    this.notaMedia = notaMedia;
+  }
+
+  public String[] getTipoCozinha() {
+    return tiposCozinha;
+  }
+
+  public void setTipoCozinha(String[] tiposCozinha) {
+    this.tiposCozinha = tiposCozinha;
+  }
+
+  public int getFaixaPreco() {
+    return faixaPreco;
+  }
+
+  public void setFaixaPreco(int faixaPreco) {
+    this.faixaPreco = faixaPreco;
+  }
+
+  public Hora getHorarioAbertura() {
+    return horarioAbertura;
+  }
+
+  public void setHorarioAbertura(Hora horarioAbertura) {
+    this.horarioAbertura = horarioAbertura;
+  }
+
+  public Hora getHorarioFechamento() {
+    return horarioFechamento;
+  }
+
+  public void setHorarioFechamento(Hora horarioFechamento) {
+    this.horarioFechamento = horarioFechamento;
+  }
+
+  public Data getDataAbertura() {
+    return dataAbertura;
+  }
+
+  public void setDataAbertura(Data dataAbertura) {
+    this.dataAbertura = dataAbertura;
+  }
+
+  public boolean isAberto() {
+    return aberto;
+  }
+
+  public void setAberto(boolean aberto) {
+
+    this.aberto = aberto;
+  }
+
+  private static String[] criarTipos(String tipos) {
+    Scanner scTiposRaw = new Scanner(tipos).useDelimiter(";");
+    int countTipos = 0;
+
+    while (scTiposRaw.hasNext()) {
+      scTiposRaw.next();
+      countTipos++;
+    }
+
+    scTiposRaw.close();
+
+    String[] tiposCozinha = new String[countTipos];
+
+    Scanner scTipos = new Scanner(tipos).useDelimiter(";");
+    for (int i = 0; scTipos.hasNext(); i++) {
+      String tipo = scTipos.next();
+      tiposCozinha[i] = tipo;
+    }
+
+    scTipos.close();
+
+    return tiposCozinha;
+  }
+
+  public static Restaurante parseRestaurante(String s) {
+    Scanner sc = new Scanner(s).useDelimiter(",");
+    int id = sc.nextInt();
+    String nome = sc.next();
+    String cidade = sc.next();
+    int capacidade = sc.nextInt();
+    double notaMedia = sc.nextDouble();
+    String tipos = sc.next();
+    String[] tiposCozinha = criarTipos(tipos);
+    int faixaPreco = sc.next().length();
+
+    String horarios = sc.next();
+    Scanner scHorarios = new Scanner(horarios).useDelimiter("-");
+
+    Hora horarioAbertura = Hora.parseHora(scHorarios.next());
+    Hora horarioFechamento = Hora.parseHora(scHorarios.next());
+
+    scHorarios.close();
+
+    Data dataAbertura = Data.parseData(sc.next());
+    boolean aberto = Boolean.parseBoolean(sc.next());
+
+    sc.close();
+
+    return new Restaurante(id, nome, cidade, capacidade, notaMedia, tiposCozinha, faixaPreco, horarioAbertura,
+        horarioFechamento, dataAbertura, aberto);
+
+  }
+
+  private String tiposCozinhaFormatado() {
+    String tiposCozinha = "";
+
+    for (int i = 0; i < this.tiposCozinha.length; i++) {
+      tiposCozinha += this.tiposCozinha[i];
+      if (i < this.tiposCozinha.length - 1) {
+        tiposCozinha += ",";
+      }
+    }
+
+    return tiposCozinha;
+  }
+
+  private String faixaPrecoFormatado() {
+    String faixaPreco = "";
+
+    for (int i = 0; i < this.faixaPreco; i++) {
+      faixaPreco += "$";
+    }
+
+    return faixaPreco;
+  }
+
+  public String formatar() {
+    return String.format("[%d ## %s ## %s ## %d ## %.1f ## [%s] ## %s ## %s-%s ## %s ## %b]", id, nome, cidade,
+        capacidade, notaMedia, tiposCozinhaFormatado(), faixaPrecoFormatado(), horarioAbertura.formatar(),
+        horarioFechamento.formatar(),
+        dataAbertura.formatar(), aberto);
+  }
+}
+
+class ColecaoRestaurantes {
+  private int tamanho;
+  private Restaurante[] restaurantes;
+
+  public void setTamanho(int tamanho) {
+    this.tamanho = tamanho;
+  }
+
+  public void setRestaurantes(Restaurante[] restaurantes) {
+    this.restaurantes = restaurantes;
+  }
+
+  public int getTamanho() {
+    return this.tamanho;
+  }
+
+  public Restaurante[] getRestaurantes() {
+    return this.restaurantes;
+  }
+
+  public void lerCsv(String path) {
+    String arquivoCSV = path;
+    try (BufferedReader br = new BufferedReader(new FileReader(arquivoCSV))) {
+      long totalLinhas = Files.lines(Paths.get(arquivoCSV)).count() - 1;
+      this.tamanho = (int) totalLinhas;
+      String linha = br.readLine();
+      int i = 0;
+      this.restaurantes = new Restaurante[(int) totalLinhas];
+      while ((linha = br.readLine()) != null) {
+        Restaurante restaurante = Restaurante.parseRestaurante(linha);
+        this.restaurantes[i] = restaurante;
+        i++;
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static ColecaoRestaurantes lerCsv() {
+    ColecaoRestaurantes c = new ColecaoRestaurantes();
+    c.lerCsv("/tmp/restaurantes.csv");
+    return c;
+  }
+
+}
+
+class Hora {
+  private int hora;
+  private int minuto;
+
+  public Hora(int hora, int minuto) {
+    this.hora = hora;
+    this.minuto = minuto;
+  }
+
+  public void setHora(int hora) {
+    this.hora = hora;
+  }
+
+  public void setMinuto(int minuto) {
+    this.minuto = minuto;
+  }
+
+  public static Hora parseHora(String s) {
+    Scanner sc = new Scanner(s).useDelimiter(":");
+    int hora = sc.nextInt();
+    int minuto = sc.nextInt();
+    sc.close();
+
+    return new Hora(hora, minuto);
+  }
+
+  public String formatar() {
+    return String.format("%02d:%02d", hora, minuto);
+  }
+}
+
+class Data {
+  private int ano;
+  private int mes;
+  private int dia;
+
+  public Data(int ano, int mes, int dia) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public void setMes(int mes) {
+    this.mes = mes;
+  }
+
+  public void setDia(int dia) {
+    this.dia = dia;
+  }
+
+  public int getAno() {
+    return this.ano;
+  }
+
+  public int getMes() {
+    return this.mes;
+  }
+
+  public int getDia() {
+    return this.dia;
+  }
+
+  public static Data parseData(String s) {
+    Scanner sc = new Scanner(s).useDelimiter("-");
+    int ano = sc.nextInt();
+    int mes = sc.nextInt();
+    int dia = sc.nextInt();
+    sc.close();
+
+    return new Data(ano, mes, dia);
+  }
+
+  public String formatar() {
+    return String.format("%02d/%02d/%02d", dia, mes, ano);
+  }
+
+  public int compareTo(Data outra) {
+    if (this.ano != outra.ano) {
+      return this.ano - outra.ano;
+    }
+    if (this.mes != outra.mes) {
+      return this.mes - outra.mes;
+    }
+    return this.dia - outra.dia;
+  }
+}
+
+class Node {
+  int h;
+  Node left;
+  Node right;
+}
+
+class AVL {
+  Node root;
+
+  void inorderTraversal() {
+
+  }
+
+  void inorderTraversal(Node node) {
+
+  }
+
+  void search() {
+
+  }
+
+  void search(Node node) {
+
+  }
+
+  void insert(int x) {
+
+  }
+
+  void insert(Node node, int x) {
+
+  }
+
+  void balance(Node node) {
+
+  }
+}
+
+class Tp04 {
+  public static Restaurante pesquisaSequencialPorId(ColecaoRestaurantes c, int id) {
+    Restaurante[] r = c.getRestaurantes();
+    for (Restaurante restaurante : r) {
+      if (restaurante != null && restaurante.getId() == id) {
+        return restaurante;
+      }
+    }
+
+    return null;
+  }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    ColecaoRestaurantes c = ColecaoRestaurantes.lerCsv();
+
+    Restaurante[] rs = new Restaurante[500];
+
+    AVL a = new AVL();
+    int id = 0;
+    id = s.nextInt();
+
+    long inicio = System.nanoTime();
+    double tempoArvore = 0.0;
+    while (id != -1) {
+      a.inserir(pesquisaSequencialPorId(c, id));
+      id = s.nextInt();
+    }
+    s.nextLine();
+    String chave;
+
+    chave = s.nextLine();
+    while (!(chave.charAt(0) == 'F' && chave.charAt(1) == 'I' && chave.charAt(2) == 'M')) {
+      System.out.print("raiz ");
+      a.pesquisar(a.root, chave);
+      chave = s.nextLine();
+    }
+
+    a.percursoEmOrdem(a.root);
+
+    long fim = System.nanoTime();
+    tempoArvore += (fim - inicio) / 1000000.0;
+
+    // try (BufferedWriter bw = new BufferedWriter(new
+    // FileWriter("897962_arvore_binaria.txt"))) {
+    // String conteudo = "897962\t" + ArvoreBinaria.compArvore + "\t" + tempoArvore
+    // + "\t" + "\n";
+    // bw.write(conteudo);
+    // } catch (IOException e) {
+    // System.out.println(e);
+    // }
+
+    s.close();
+  }
+}
